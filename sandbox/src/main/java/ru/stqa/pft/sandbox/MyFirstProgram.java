bc59c10fc6f16f82f791efb2cf58738fef073d58
prichislov <prychyslov@gmail.com>
2017-06-13 15:29:03 +0300

    changes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,7 +2,16 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram{
 
-    public static void main(String[]args){
-        System.out.println("Hello, World");
+    public static void main(String[]args) {
+        hello("Hallo word!!!");
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
     }
+    public static void hello(String somebody ){
+        System.out.println(somebody);
+    }
+    public static double area( double len){
+        return len * len;
+    }
+
 }
