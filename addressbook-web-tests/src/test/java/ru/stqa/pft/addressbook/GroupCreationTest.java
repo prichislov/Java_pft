e8f9fb448a41a6f07019b0508edaf5ce54728d02
prichislov <prychyslov@gmail.com>
2017-06-19 18:54:00 +0300

    upgrade code

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -22,10 +22,7 @@ public class GroupCreationTest {
         System.setProperty("webdriver.gecko.driver","C:\\Selenium\\geckodriver.exe");
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTest() {
+
         wd.get("http://localhost/addressbook/group.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -34,8 +31,11 @@ public class GroupCreationTest {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.xpath("//div[@id='content']/form/input[4]")).click();
+    }
+    
+    @Test
+    public void testGroupCreation() {
+
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
         wd.findElement(By.name("group_name")).sendKeys("groupTest");
